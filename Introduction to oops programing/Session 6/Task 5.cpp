95b632490e53f8be1614520192a642b30e8dd5af
Jonal1122 <jiyanpatidar077@gmail.com>
2026-07-06 23:46:05 +0530

    Add files via upload

---
+++ Introduction to oops programing/Session 6/Task 5.cpp
@@ -0,0 +1,7 @@
+// Encapsulation means hiding data using private and accessing it with public methods.
+
+Example: In WhatsApp, your phone number is not directly changed from outside. You update it through settings.
+
+Abstraction means showing only important details and hiding internal working.
+
+Example: In Instagram, when you upload a story, you only click upload. You do not see how Instagram stores and sends it to followers.
